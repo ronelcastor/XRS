620c4791da763e295f6440ff4dea5983d092e0f0
Ronel Castor <Ronel.Castor@xerox.com>
2018-07-06 22:20:02 +0800

    init CTRS

---
+++ CTRSv3/principal.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CTRSv3
+{
+    public partial class principal : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
